66d584eadc4923c01eb12bfc45bba699447bc553
maliksbveo2-sys <maliksbveo2@gmail.com>
2026-03-20 00:56:17 +0500

    Update server.js

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.post("/generate", async (req, res) => {
   res.send(Buffer.from(audio));
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
